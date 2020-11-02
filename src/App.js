8fefebc4af02e54415447ee029b00794f6da2c53
YousefBunashi <bunashi88@hotmail.com>
2020-11-02 12:50:15 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+// import items from "./items.js";
+
 const styles = {
   text: {
     textAlign: "center",
   },
-  bbqImage: {
+  shopImage: {
     display: "block",
     marginLeft: "auto",
     marginRight: "auto",
     width: "50%",
   },
+  bbqImage: {
+    width: "200px",
+    height: "200px",
+  },
+  list: {
+    alignItems: "centr",
+    justfycontent: "center",
+    display: "flex",
+  },
+  item: {
+    margin: "20px",
+  },
 };
 
 function App() {
+  const itemList = items.map((item) => (
+    <div style={styles.item}>
+      <img style={styles.itemImage} src={item.image} alt={item.name} />
+      <p style={styles.text}>{item.name}</p>
+      <p style={styles.text}>{item.price} KD</p>
+    </div>
+  ));
+
   return (
     <div>
-      <h1 style={styles.text}>Second Best Burger in Town</h1>
-      <img
-        style={styles.bbqImage}
-        src="https://thehuntr.com/wp-content/uploads/2017/11/BBQ-Box-Dubai-2-663x440.jpg"
-      />
+      <div>
+        <h1 style={styles.text}>Second Best Burger in Town</h1>
+        <img
+          style={styles.shopImage}
+          src="https://thehuntr.com/wp-content/uploads/2017/11/BBQ-Box-Dubai-2-663x440.jpg"
+          alt="BBQ SHOP"
+        />
+      </div>
+      <div style={styles.list}>{itemList}</div>
     </div>
   );
 }
---
+++ src/items.js
@@ -0,0 +1,21 @@
+const items = [
+  {
+    name: "fmailly box",
+    price: 60,
+    image:
+      "https://thehuntr.com/wp-content/uploads/2017/11/BBQ-Box-Dubai-2-663x440.jpg",
+  },
+  {
+    name: "Hot dog box",
+    price: 30,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQyDL1Gc37CZibqErcZcC35ph3XaYHP2KABEg&usqp=CAU",
+  },
+  {
+    name: "slider box",
+    price: 30,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRgDPnLatcWgH3g7kiQ8SIoUEVKLK4lWR5cw&usqp=CAU",
+  },
+];
+export default items;
