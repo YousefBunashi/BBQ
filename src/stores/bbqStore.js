b1dabe9a93fedecde26a500f515112aeea9628f5
YousefBunashi <bunashi88@hotmail.com>
2020-11-23 12:40:20 +0300

    update done

--- src/stores/bbqStore.js
+++ src/stores/bbqStore.js
@@ -15,22 +15,44 @@ class BbqsStore {
     });
   }
   fetchBbqs = async () => {
-    const response = await axios.get("http://localhost:8000/bbqs");
-    this.bbqs = response.data;
+    try {
+      const response = await axios.get("http://localhost:8000/bbqs");
+      this.bbqs = response.data;
+    } catch (error) {
+      console.error("BbqsStore -> fetchBbqs -> error", error);
+    }
   };
 
-  createBbq = (newbbq) => {
+  updateBbq = async (updatedBbq) => {
+    try {
+      await axios.put(
+        `http://localhost:8000/bbqs/${updatedBbq.id}`,
+        updatedBbq
+      );
+      const bbq = this.bbqs.find((bbq) => bbq.id === updatedBbq.id);
+      for (const key in bbq) bbq[key] = updatedBbq[key];
+    } catch (error) {
+      console.log("BbqsStore -> updateBbq -> error", error);
+    }
+  };
+  createBbq = async (newbbq) => {
     // bbq.id = this.bbqs[this.bbqs.length - 1].id + 1;
     // bbq.slug = slugify(bbq.name);
-    this.bbqs.push(newbbq);
+    try {
+      const res = await axios.post("http://localhost:8000/bbqs", newbbq);
+      this.bbqs.push(res.data);
+    } catch (error) {
+      console.log("bbqstore -> createBbq -> error", error);
+    }
   };
-  DeleteButton = (bbqId) => {
-    this.bbqs = this.bbqs.filter((bbq) => bbq.id !== bbqId);
-  };
-  updateBbq = (updatedBbq) => {
-    const bbq = this.bbqs.find((bbq) => bbq.id === updatedBbq.id);
-    for (const key in bbq) bbq[key] = updatedBbq[key];
-    bbq.slug = slugify(bbq.name);
+
+  DeleteButton = async (bbqId) => {
+    try {
+      await axios.delete(`http://localhost:8000/bbqs/${bbqId}`);
+      this.bbqs = this.bbqs.filter((bbq) => bbq.id !== +bbqId);
+    } catch (error) {
+      console.log("BbqsStore -> deletebbq -> error", error);
+    }
   };
 }
 
